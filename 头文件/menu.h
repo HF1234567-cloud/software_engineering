b48c6a8e744069d253e51e01c35835ea5a60a13b
HF <useremail@qq.com>
2025-10-20 17:28:12 +0800

    上传menu头文件

---
+++ 头文件/menu.h
@@ -0,0 +1,16 @@
+
+#ifndef MENU_H
+#define MENU_H
+int outputmenu();//声明output函数
+void add();//声明添加函数
+void query();//声明查询函数
+void exitapp();//退出程序
+void logon();//上机
+void settle();//退机
+void addMoney();//充值
+void refundMoney();//退费
+void annul();//注销
+void renew();//重新激活
+void statistic();//统计
+void printAllCard();
+#endif
